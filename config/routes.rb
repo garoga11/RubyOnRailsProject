875132eadc9e81cabc08354a0050737e89d7dd66
garoga11 <rojasgabriela901@gmail.com>
2021-02-03 12:01:10 -0700

    Bootstrap was added

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   
   get '/products', to: 'products#index'
+  post '/products/new', to: 'products#new'
   #resources :products
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
