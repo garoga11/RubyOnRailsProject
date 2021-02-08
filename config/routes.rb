39e708f74788f37c4a1276f083e1cae9819746b6
garoga11 <rojasgabriela901@gmail.com>
2021-02-08 07:15:57 -0700

    adding post route

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   
   get '/products', to: 'products#index'
-  post '/products/new', to: 'products#new'
+  get '/products/new', to: 'products#new'
+  post '/products', to: 'products#create'
   #resources :products
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
